450665c14ea65aaccbaae5a0ad56e3cc09f83e87
Sagar Sharma <sharma9785sagar007@gmail.com>
2022-12-05 13:09:37 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: invalid_required_named_param, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
-import 'package:flutterui/home_page.dart';
+import 'package:flutterui/pages/home_page.dart';
+import 'package:flutterui/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +13,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
