cd35b8c3d602a2c448d537f9a890a2584722c0cc
Sagar Sharma <sharma9785sagar007@gmail.com>
2022-12-05 10:15:20 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          // ignore: avoid_unnecessary_containers
+          child: Container(
+            child: const Text("Welcome to Flutter"),
+          ),
+        ),
+      ),
+    );
+  }
+}
