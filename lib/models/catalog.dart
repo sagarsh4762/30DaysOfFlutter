bb7998cfcbee0588ac9551fb354401acd08c231d
Sagar Sharma <sharma9785sagar007@gmail.com>
2022-12-06 12:21:41 +0530

    Day 9 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "Iphone 12 Pro",
+        desc: "Apple iphone 12th generation",
+        price: 999,
+        color: "#33505a",
+        image:
+            "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-model-unselect-gallery-1-202209?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660689596976")
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,29 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutterui/models/catalog.dart';
 import 'package:flutterui/widgets/drawer.dart';
+import 'package:flutterui/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Sagar";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Catalog App",
         ),
       ),
-      body: Center(
-        // ignore: avoid_unnecessary_containers
-        child: Container(
-          child: Text(context.runtimeType.toString()),
-        ),
+      body: ListView.builder(
+        itemCount: dummyList.length,
+        itemBuilder: (context, index) {
+          return ItemWidget(
+            item: dummyList[index],
+          );
+        },
       ),
       drawer: MyDrawer(),
     );
